820d09f59e475d679d160789313bd9d2e25e032b
kauanhen02 <kauansjx31@gmail.com>
2024-05-06 09:55:42 -0300

    first commit

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
 const carousel = document.querySelector(".carousel")
-const carouselItems = document.querySelectorAll(".carousel-item")
 const prevBtn = document.querySelector(".prev")
 const nextBtn = document.querySelector(".next")
+const totalImages = document.querySelectorAll(".carousel-item").length
+let position = 0
 
-let currentIndex = 0
+nextBtn.addEventListener("click", () => {
+  position -= carousel.offsetWidth / 3 // Move para a próxima imagem
 
-function showImage(index) {
-  carousel.style.transform = `translateX(-${index * 100}%)`
-}
+  if (position <= -(carousel.offsetWidth * (totalImages - 3))) {
+    // Se a próxima imagem for a última, volte para a primeira
+    position = 0
+  }
 
-function prevImage() {
-  currentIndex =
-    (currentIndex - 1 + carouselItems.length) % carouselItems.length
-  showImage(currentIndex)
-}
+  carousel.style.transition = "transform 0.5s ease" // Adiciona transição suave
+  carousel.style.transform = `translateX(${position}px)`
+})
 
-function nextImage() {
-  currentIndex = (currentIndex + 1) % carouselItems.length
-  showImage(currentIndex)
-}
+prevBtn.addEventListener("click", () => {
+  position += carousel.offsetWidth / 3 // Move para a imagem anterior
 
+  if (position > 0) {
+    // Se estiver na primeira imagem e clicar para ir para trás, vá para a última
+    position = -(carousel.offsetWidth * (totalImages - 3))
+  }
 
+  carousel.style.transition = "transform 0.5s ease" // Adiciona transição suave
+  carousel.style.transform = `translateX(${position}px)`
+})
+
+// Adiciona um ouvinte de evento de transição para verificar quando a transição termina
+carousel.addEventListener("transitionend", () => {
+  if (
+    position === 0 ||
+    position <= -(carousel.offsetWidth * (totalImages - 3))
+  ) {
+    // Se a posição estiver na primeira ou última imagem, remova a transição
+    carousel.style.transition = "none"
+  }
+})
